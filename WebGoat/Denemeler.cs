54975b50216392a83078c2212137f260fab185e8
Emre Sakarya <emresakarya@attackflow.com>
2018-10-03 10:07:06 +0300

    Initial commit

---
+++ WebGoat/Denemeler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace OWASP.WebGoat.NET
+{
+    public class Denemeler
+    {
+        public void ParolaGir(string state)
+        {
+            string xml = "";
+
+            XmlDocument xDoc = new XmlDocument();
+            //xDoc.LoadXml(xml);
+            //XmlNodeList list = xDoc.SelectNodes("//salesperson[state='" + state + "']");
+            //if (list.Count > 0)
+            //{
+
+            //}
+        }
+    }
+}
